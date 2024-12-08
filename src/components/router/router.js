8017913afbe612bf36ecf8d2ffb0e98ecb898965
buinam03 <thanhks161@gmail.com>
2024-12-09 01:25:40 +0700

    add title

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -1,19 +1,20 @@
-import { createRouter, createWebHistory } from "vue-router";
-import huhCaiqq from "../huh.vue";
-
+import { createRouter, createWebHashHistory } from 'vue-router';
+import huhCaiqq from '../huh.vue';
 
 const routes = [
-{
+  {
     path: '/',
     name: 'huh',
     component: huhCaiqq,
-}
-
-]
+    meta: {
+        title: 'Nhìn qq',
+    }
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHashHistory(), // Sử dụng createWebHashHistory thay vì createWebHistory
+  routes
 });
 
 export default router;
